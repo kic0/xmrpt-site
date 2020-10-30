e994ca57ccc9d03549952af01acb88b373c9c7b5
Rolando Reset <roltel@roltel.net>
2020-10-30 18:15:43 +0000

    Update minar.php
    Meter instruções para o xmr e tirar as antigas que esavam obsoletas

--- minar.php
+++ minar.php
@@ -1,8 +1,6 @@
 <html>
 <?php include("header.php");?>
-
 <center>
-
 <div>
 <table style="width: 800px;">
 <tbody>
@@ -10,63 +8,57 @@
 <td style="width: 800px;">
 <div class="entry-content">
 <p><strong>Monero</strong> A minera&ccedil;&atilde;o de Monero pode ser um empreendimento lucrativo devido ao seu algoritmo criptogr&aacute;fico que &eacute; resistente &agrave; minera&ccedil;&atilde;o da ASIC. Isso significa que qualquer pessoa com CPU ou GPU pode minar Monero.</p>
-
-<iframe width="100%">https://new.xmr.pt/</iframe>
+<iframe width="100%" height="150">https://new.xmr.pt/</iframe>
 <p>Este guia ir&aacute; mostrar-lhe quais programas de software precisa e como configurar esses programas para que possa explorar o Monero em apenas alguns minutos.</p>
 <p>Pode explorar o Monero e com diferentes qual softwares para minerar, dependendo do <em>hardware</em>:</p>
-<p><strong>Placas Gr&aacute;ficas AMD</strong></p>
-<ul>
-<li>XMR Stak<img class=" wp-image-222 alignright" style="float: right;" src="https://mbcoin.pt/wp-content/uploads/2018/01/xmr.png" sizes="(max-width: 228px) 100vw, 228px" srcset="https://mbcoin.net/blog/wp-content/uploads/2018/01/xmr.png 450w, https://mbcoin.net/blog/wp-content/uploads/2018/01/xmr-150x150.png 150w, https://mbcoin.net/blog/wp-content/uploads/2018/01/xmr-300x298.png 300w" alt="" width="228" height="226" /><br /> 2% Dev Fee (a menos que seja compilado)</li>
-<li>Wolf&rsquo;s Miner<br /> No Dev Fee (n&atilde;o como otimizado)</li>
-</ul>
-<p><strong>Placas Gr&aacute;ficas NVIDIA</strong></p>
-<ul>
-<li>XMR Stak 2% Dev Fee (a menos que seja compilado)</li>
-</ul>
-<p><strong>CPU (Intel / AMD)</strong></p>
-<ul>
-<li>Monero Spelunker</li>
-<li>XMR Stak</li>
-<li>Wolfs Miner</li>
-</ul>
-<p><strong>Instalar software de minera&ccedil;&atilde;o</strong></p>
-<p>XMR Stak lan&ccedil;ou recentemente uma atualiza&ccedil;&atilde;o que permite aos utilizadores simplesmente baixar um instalador independentemente do hardware que se pretende usar. Portanto, utilizaremos o XMR Stak neste guia, pois sempre produz bons resultados e &eacute; muito amig&aacute;vel.</p>
-<p>XMR Stak pode ser baixado neste <a href="https://github.com/fireice-uk/xmr-stak/releases">link</a>. Por favor, note que alguns softwares antiv&iacute;rus identificam softwares de minera&ccedil;&atilde;o como arquivos maliciosos. Para estar seguro, sempre verifique se est&aacute; a fazer o download da fonte oficial. <img class="size-medium wp-image-255 alignleft" style="float: right;" src="https://mbcoin.pt/wp-content/uploads/2018/01/screenshot.90-300x179.jpg" sizes="(max-width: 300px) 100vw, 300px" srcset="https://mbcoin.net/blog/wp-content/uploads/2018/01/screenshot.90-300x179.jpg 300w, https://mbcoin.net/blog/wp-content/uploads/2018/01/screenshot.90-768x459.jpg 768w, https://mbcoin.net/blog/wp-content/uploads/2018/01/screenshot.90-1024x612.jpg 1024w, https://mbcoin.net/blog/wp-content/uploads/2018/01/screenshot.90.jpg 1281w" alt="" width="300" height="179" /></p>
-<p>A vers&atilde;o mais recente do XMR Stak torna muito f&aacute;cil a configura&ccedil;&atilde;o. Vamos assumir que os usu&aacute;rios est&atilde;o em ambiente Windows, mas as vers&otilde;es Linux e MacOS tamb&eacute;m est&atilde;o dispon&iacute;veis. Basta fazer o download no link acima, descompactar para um local de arquivo e executar o execut&aacute;vel.</p>
 <p>&nbsp;</p>
-<p><strong>Ligue-se &agrave; Mining Pool</strong></p>
-<p>Depois de executar a aplica&ccedil;&atilde;o, ir&aacute; perguntar se minar Monero ou Aeon. N&oacute;s escolheremos o Monero. Ent&atilde;o, pede-nos para indicar no endere&ccedil;o da piscina (pool). &Eacute; importante envolver-se num bom grupo de minera&ccedil;&atilde;o, pois a minera&ccedil;&atilde;o a solo pode render pouca ou nenhuma recompensa sem um hashrate extremamente alto. As <em>&ldquo;</em>piscinas&rdquo; geralmente v&ecirc;m com uma pequena taxa, mas justificadas para se obter uma renda consistente.</p>
-<p><img class=" wp-image-256 alignright" src="https://mbcoin.pt/wp-content/uploads/2018/01/screenshot.92-300x157.jpg" sizes="(max-width: 493px) 100vw, 493px" srcset="https://mbcoin.net/blog/wp-content/uploads/2018/01/screenshot.92-300x157.jpg 300w, https://mbcoin.net/blog/wp-content/uploads/2018/01/screenshot.92-768x402.jpg 768w, https://mbcoin.net/blog/wp-content/uploads/2018/01/screenshot.92.jpg 977w" alt="" width="493" height="258" /></p>
+<p><img class=" wp-image-222 alignright" style="float: right;" src="https://mbcoin.pt/wp-content/uploads/2018/01/xmr.png" sizes="(max-width: 228px) 100vw, 228px" srcset="https://mbcoin.net/blog/wp-content/uploads/2018/01/xmr.png 450w, https://mbcoin.net/blog/wp-content/uploads/2018/01/xmr-150x150.png 150w, https://mbcoin.net/blog/wp-content/uploads/2018/01/xmr-300x298.png 300w" alt="" width="228" height="226" /></p>
+<ul>
+<li>&nbsp;<a class="C1 hov" href="https://github.com/xmrig/xmrig/releases" target="_blank" rel="noopener">XMRig</a>: para minar em CPU and GPU (Nvidia, AMD)</li>
+<li>&nbsp;<a class="C1 hov" href="https://github.com/fireice-uk/xmr-stak/releases" target="_blank" rel="noopener">XMR-Stak/RX</a>: para minar com CPU</li>
+<li><a class="C1 hov" href="https://github.com/MoneroOcean/meta-miner" target="_blank" rel="noopener">&nbsp;mm.js</a>: for algo switching miner wrapper (advanced)<br /><br /></li>
+</ul>
+<p>Pode tamb&eacute;m usar <a class="C1 hov" href="https://github.com/MoneroOcean/xmrig-proxy/releases" target="_blank" rel="noopener">algo switching mining proxy</a> se tiver muitos miners.</p>
+<p>&nbsp;</p>
+<p>Dependendo do software que usar, cada ficheiro de configura&ccedil;&atilde;o varia mas pedir&atilde;o basicamente a mesma informa&ccedil;&atilde;o.</p>
+<p><strong>Endere&ccedil;o da carteira de Monero</strong><br />&Agrave;s vezes ser&aacute; referido como <em>username </em>mas &eacute; melhor consultar a configura&ccedil;&atilde;o.</p>
+<p><strong>Endere&ccedil;o da Pool de Minera&ccedil;&atilde;o</strong><br />O minerador vai querer o <em>url</em> e <em>porta</em> assim: pool.xmr.pt:5555</p>
+<p>&nbsp;</p>
+<div class="helpcontent">
+<table class="txtsmall C3l">
+<tbody>
+<tr>
+<td>
+<p>Port descriptions:</p>
+<ul>
+<li><strong>3333</strong>: CPU e GPU antigos</li>
+<li><strong>5555</strong>: Modernos CPU/GPU</li>
+<li><strong>7777</strong>: Harware de topo</li>
+<li><strong>9000</strong>: SSL/TLS</li>
+</ul>
+</td>
+<td>
+<p>&nbsp;</p>
+</td>
+</tr>
+</tbody>
+</table>
+<p>&nbsp;</p>
+<p><strong>Campos opcionais</strong><br />Pode alterar o nome da esta&ccedil;&atilde;o mineradora ou a dificuldade no ficheiro de configura&ccedil;&atilde;o.</p>
+<p>Nome de esta&ccedil;&atilde;o mineradora<br /><em>(e.x. xmrig.exe -u 43T...sUW -p <strong>mina1</strong>)</em></p>
+<p>Dificuldade fixa para a esta&ccedil;&atilde;o 128000<br /><em>(e.x. xmrig.exe -u 43T...sUW<strong>+128000</strong> -p mina1)</em></p>
+</div>
 <p>Em Portugal existe uma <em>pool</em> bastante activa, com uma comunidade participativa e dispon&iacute;vel. Chama-se:</p>
 <p><a href="https://pool.xmr.pt">XMRPT</a></p>
-<p>Servidores: Portugal<br /> Mineradores: ~150<br /> Dev fee: 0.4 %<br /> Pag. m&iacute;nimo: 0.2 xmr</p>
-<p><a href="http://minexmr.com/#">MineXMR</a></p>
-<p>Servidores: Fran&ccedil;a, Alemanha, Canada<br /> Mineradores: ~20000<br /> Dev fee: 1%<br /> Pagamento m&iacute;nimo: 0.3 xmr</p>
-<p><a href="https://supportxmr.com">SUPPORTXMR</a></p>
-<p>Servidores: Fran&ccedil;a, Alemanha<br /> Mineradores: ~5000<br /> Dev fee: 0.6%<br /> Pagamento m&iacute;nimo: 0.1 xmr</p>
-<p>Para este guia, usaremos o software na pool portuguesa de Monero, pois tem as taxas mais baixas, e &eacute; o servidor mais pr&oacute;ximo para mim (vai querer escolher o servidor mais pr&oacute;ximo da sua localiza&ccedil;&atilde;o para minimizar a lat&ecirc;ncia). A seguinte imagem mostra os detalhes da conex&atilde;o para o <a href="https://pool.xmr.pt">XMRPT</a> que iremos inserir na aplica&ccedil;&atilde;o.</p>
-<p><img class="size-medium wp-image-257 alignleft" src="https://mbcoin.pt/wp-content/uploads/2018/01/screenshot.91-300x157.jpg" sizes="(max-width: 300px) 100vw, 300px" srcset="https://mbcoin.net/blog/wp-content/uploads/2018/01/screenshot.91-300x157.jpg 300w, https://mbcoin.net/blog/wp-content/uploads/2018/01/screenshot.91-768x402.jpg 768w, https://mbcoin.net/blog/wp-content/uploads/2018/01/screenshot.91.jpg 977w" alt="" width="300" height="157" /></p>
-<p>&nbsp;</p>
-<p>Para este uso, vou inserir na aplica&ccedil;&atilde;o o servidor que desejo usar com o n&uacute;mero da porta. Por exemplo, vou usar &lsquo;pool.xmr.pt:3333&rsquo;.</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p><strong>Endere&ccedil;o da Carteira de Monero </strong></p>
-<p>Uma vez que inseriu os dados da pool &eacute;-lhe pedido o endere&ccedil;o da sua carteira (ou o login do pool se estiver usando algo como o Supernova). Por favor, veja este guia do Monero para configurar uma carteira. Depois de ter o endere&ccedil;o da sua carteira, insira-a na aplica&ccedil;&atilde;o. Para a senha no nosso caso, podemos simplesmente clicar em enter. A pool XMRPT suporta TLS / SSL, mas neste caso n&atilde;o iremos usar. N&atilde;o queremos usar o NiceHash, e n&atilde;o queremos usar v&aacute;rias pools neste momento (embora possa ser ben&eacute;fico configurar pools adicionais com troca autom&aacute;tica apenas no caso delas fique offline). Todos esses valores podem ser ajustados no arquivo de configura&ccedil;&atilde;o mais tarde.</p>
-<p>Agora, o aplicativo ser&aacute; executado com base nas GPUs que instalou. Pode usar os comandos na parte inferior para ver o status de seus mineradores. Clique em &lsquo;h&rsquo; para ver o hashrate, &lsquo;r&rsquo; para ver os resultados de seus hashes ou &lsquo;c&rsquo; para verificar a conex&atilde;o ao pool.</p>
-<p>&nbsp;</p>
-<p><strong>Ficheiro de configura&ccedil;&atilde;o</strong></p>
-<p>Se quiser minar com o seu CPU, vai ainda necessitar de configurar o arquivo de configura&ccedil;&atilde;o do CPU antes que possa faz&ecirc;-lo. Para fazer isso, clique no documento de texto da CPU que &eacute; gerado.</p>
-<p>Uma vez no documento de texto, queremos adicionar as threads da CPU ao documento de texto para que ele utilize esses cores para minar. Para o meu caso, eu tenho um chip CPU que usa 2 n&uacute;cleos, ent&atilde;o no meu documento de texto da CPU, n&atilde;o necessito de adicionar nada:</p>
-<p><img class="wp-image-258 aligncenter" src="https://mbcoin.pt/wp-content/uploads/2018/01/screenshot.94-1024x615.jpg" sizes="(max-width: 605px) 100vw, 605px" srcset="https://mbcoin.net/blog/wp-content/uploads/2018/01/screenshot.94-1024x615.jpg 1024w, https://mbcoin.net/blog/wp-content/uploads/2018/01/screenshot.94-300x180.jpg 300w, https://mbcoin.net/blog/wp-content/uploads/2018/01/screenshot.94-768x461.jpg 768w, https://mbcoin.net/blog/wp-content/uploads/2018/01/screenshot.94.jpg 1279w" alt="" width="605" height="363" /></p>
-<p>&nbsp;</p>
-<p><img class="size-medium wp-image-259 alignright" style="float: right;" src="https://mbcoin.pt/wp-content/uploads/2018/01/screenshot.95-300x180.jpg" sizes="(max-width: 300px) 100vw, 300px" srcset="https://mbcoin.net/blog/wp-content/uploads/2018/01/screenshot.95-300x180.jpg 300w, https://mbcoin.net/blog/wp-content/uploads/2018/01/screenshot.95-768x461.jpg 768w, https://mbcoin.net/blog/wp-content/uploads/2018/01/screenshot.95-1024x615.jpg 1024w, https://mbcoin.net/blog/wp-content/uploads/2018/01/screenshot.95.jpg 1279w" alt="" width="300" height="180" />Se pretender usar mais CPUs na minera&ccedil;&atilde;o, tem de inserir uma linha por cada n&uacute;cleo. Eu tamb&eacute;m uso este PC ao longo do dia, ent&atilde;o eu quero deixar alguns <em>cores</em> livres para minhas atividades di&aacute;rias. Para sua configura&ccedil;&atilde;o, pode querer executar todos os n&uacute;cleos (embora o congelamento &agrave;s vezes ocorra se fizer isso). Pode tentar ajustar o prefetch, alterando-o para falso, mas n&atilde;o h&aacute; muitas op&ccedil;&otilde;es aqui para otimizar a CPU para minera&ccedil;&atilde;o. Depois de fazer essas altera&ccedil;&otilde;es, reinicie a aplica&ccedil;&aacute;o e deve notar um aumento no seu hashrate, pois ele deve estar usando sua GPU e CPU. Tamb&eacute;m pode verificar isso olhando a carga em do seu CPU com um aplicativo como OpenHardwareMonitor.</p>
-<p>E &eacute; isso! Agora est&aacute; extraindo Monero com suas GPUs e CPU&rsquo;s. Se precisar fazer altera&ccedil;&otilde;es ou ajustes devido a falhas ou mudan&ccedil;as na sua <em>pool</em> de minera&ccedil;&atilde;o, configura&ccedil;&otilde;es adicionais podem ser encontradas no ficheiro de configura&ccedil;&atilde;o.</p>
+<p>Servidores: Portugal<br /> Mineradores: ~150<br /> Dev fee: 0.4 %<br /> Pag. m&iacute;nimo: 0.2 xmr&nbsp;</p>
 </div>
 </td>
 </tr>
 </tbody>
 </table>
 </div>
+</center><center>
+</center>
 <div>&nbsp;</div>
 
 
